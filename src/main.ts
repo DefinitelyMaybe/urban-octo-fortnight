6bde2a7c5bdac0b663debfffff060ab038cc5c87
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2021-06-27 13:55:39 +1200

    add UUID log

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
+// In Deno Deploy, the BroadcastChannel API provides a communication
+// mechanism between the various instances; a simple message bus 
+// that connects the various Deploy instances world wide.
+
 addEventListener("fetch", (event) => {
   const response = new Response("Hello World!", {
     headers: { "content-type": "text/plain" },
   });
+  console.log(crypto.randomUUID());
   event.respondWith(response);
 });
